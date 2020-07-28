c1a16c8cbd24b96c4938a0b0e5a24f04c9d5d9f7
rongyi <rongyi@onchain.com>
2020-07-28 17:06:56 +0800

    Update comment

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	r := gin.Default()
 
-	// FIXME: pc should init with detail option, this is just the demo, we ignore all the config detail info
+	// FIXME: service config should init with detail option, this is just the demo, we ignore all the config detail info
 	sc := service.NewConfig()
 
 	if err := meta.RegistPath(r, sc); err != nil {
