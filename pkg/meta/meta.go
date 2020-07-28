1291a357e7727b11dccdda92083b63e2a20ea14e
rongyi <rongyi@onchain.com>
2020-07-28 14:22:09 +0800

    Update meta

--- pkg/meta/meta.go
+++ pkg/meta/meta.go
@@ -7,12 +7,12 @@ type AddonConfigSteps struct {
 
 // Step define each step's detail
 type Step struct {
-	Index       int32                  `json:"index"`       // index of total steps, NOTE: start from 1, not 0
-	Name        string                 `json:"name"`        // current step name
-	Description string                 `json:"description"` // more verbose description
-	Params      map[string]interface{} `json:"params"`      // request param
-	Path        string                 `json:"path"`        // the relative HTTP path for current steps
-	IsTx        bool                   `json:"tx"`          // is this step a transaction? addon server will use different way to process transaction step and none transaction step, see readme for detail description
+	Index       int32       `json:"index"`       // index of total steps, NOTE: start from 1, not 0
+	Name        string      `json:"name"`        // current step name
+	Description string      `json:"description"` // more verbose description
+	Params      interface{} `json:"params"`      // request param
+	Path        string      `json:"path"`        // the relative HTTP path for current steps
+	IsTx        bool        `json:"tx"`          // is this step a transaction? addon server will use different way to process transaction step and none transaction step, see readme for detail description
 
 	IsRollbackTx []bool   `json:"is_rollback_tx_lst,omitempty"` // one step may need multiple rollback steps, the length of the array indicate the total rollback steps, and each value indicate wether the step is a transaction step
 	RollbackPath []string `json:"rollback_path_lst,omitempty"`  // rollback request HTTP path
